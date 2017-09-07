97482bf65f6a74abb012c9f8a8e080b6732d622e
wanghang <100349018wh@gmail.com>
2017-09-07 21:35:28 +0800

    增加了k个最大子数组的和

--- src/MaxSub.java
+++ src/MaxSub.java
@@ -10,6 +10,11 @@ public class MaxSub {
 	public int maxSubArray(int[] nums, int k) {
         // write your code here
 		
+		if(k > nums.length){
+		    return 0;
+		}
+		
+		int min = Integer.MIN_VALUE;
 		/** localMax[i][j]记录数组前j个数的子数组被划分为i组， 且必须包含第j个数的最大和  **/
 		int[][] localMax = new int[k + 1][nums.length + 1];
 		
@@ -17,12 +22,32 @@ public class MaxSub {
 		int[][] globalMax = new int[k + 1][nums.length + 1];
 		
 		/**
-		 * 最优子结构 localMax[i][j] = Max(localMax[i][j - 1] + )
-		 * 
-		 * 
+		 * 最优子结构 localMax[i][j] = Max(localMax[i][j - 1] , globalMax[i-1][j-1]) + nums[j]
+		 *        globalMax[i][j] = Max(localMax[i][j], globalMax[i][j -1]);
+		 * 边界条件： localMax[0][j] = min (数值前j个数被划分为0组)
+		 *            globalMax[0][j] = 0;
+		 *        localMax[i][i-1] = min (数组前0个数被划分为i组)
+		 *        globalMax[i][i-1] = 0
+		 *        当i = j时， globalMax[i][j] = localMax[i][j]
 		 */
+		 for(int j = 0; j < nums.length + 1; j++){
+			 localMax[0][j] = min;
+			  globalMax[0][j] = 0;
+		 }
 		
-		return 0;
+		 for(int i = 1; i < k + 1; i++){
+			 localMax[i][i-1] = min;
+			 globalMax[i][i-1] = 0;
+			 for(int j = i; j < nums.length + 1; j++){ //j<i,不能被划分成i个子数组
+				 localMax[i][j] = Math.max(localMax[i][j - 1], globalMax[i-1][j-1]) + nums[j-1];
+				 if(i == j){
+				     globalMax[i][j] = localMax[i][j];
+				 }else{
+				     globalMax[i][j] = Math.max(localMax[i][j], globalMax[i][j -1]);
+				 }
+			 }
+		 }	 
+		return globalMax[k][nums.length];
     }
 	
 	
