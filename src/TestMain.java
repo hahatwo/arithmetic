462175c326f7bb54138e13354c7a0ef2c13b2c0e
wanghang <100349018wh@gmail.com>
2017-12-12 22:14:26 +0800

    解决冲突

--- src/TestMain.java
+++ src/TestMain.java
@@ -3,16 +3,10 @@ public class TestMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int count = 0;
-		SearchMatrixII sea = new SearchMatrixII();
-		int[][] matrix = {{62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80},
-				{63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81},
-				{64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82},
-				{65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83},
-				{66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84},
-				{67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85}
-		};
-		count = sea.searchMatrix(matrix, 81);	
-		System.out.println(count);
+		RotatedSortedArray sort = new RotatedSortedArray();
+		int[] A = {6,8,9,1,3,5};
+		
+		int index = sort.search(A, 5);
+		System.out.println(index);
 	}
 }
